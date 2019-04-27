d9922ea780061c8d0e1995232a22fc3740085057
Justin Thomas <justamnew@gmail.com>
2019-04-26 22:14:10 -0600

    GUI Time!
    Made final design of the GUI. Starting to implement listeners for buttons, sliders, etc.

--- PhotoCatcher.java
+++ PhotoCatcher.java
@@ -10,7 +10,11 @@ import java.awt.event.*;
 public class PhotoCatcher extends JPanel{
 	
 	private static ArrayList<Picture> listOfPictures = new ArrayList<Picture>();
-	private static String[] listOfPictureFiletypes = {"jpg","jpeg","png","tiff","gif","bmp"};
+	private static String[] arrayOfPictureFiletypes = {"jpg","jpeg","png","tiff","gif","bmp"};
+	private static ArrayList<String> listOfPictureFiletypes = new ArrayList<String>(Arrays.asList(arrayOfPictureFiletypes));
+	
+	private static ArrayList<File> listOfDirectories;
+	
 	
 	private static JFrame jf;
 	private static PhotoCatcher catcher;
@@ -25,6 +29,21 @@ public class PhotoCatcher extends JPanel{
 	
 	private JButton addExtension;
 	private JButton removeExtension;
+	
+	
+	private String[] searchTypeStrings = {"Duplicates","Similar"};
+	private JComboBox<String> searchType;
+	
+	private String[] algorithmTypeStringsDuplicates = {"Full Picture","Half Picture","Filehash"};
+	private String[] algorithmTypeStringsSimilar = {"Full Picture","Half Picture"};
+	private String[][] algorithmTypeOptions = {algorithmTypeStringsDuplicates, algorithmTypeStringsSimilar};
+	private int algorithmTypeInt = 0;
+	private JComboBox<String> algorithmType;
+	
+	private JSlider rgbGraceValue;
+	private JSlider percentSimilarity;
+	
+	private JCheckBox hardCompare;
 	
 	public static void main(String[] args){
 		//Set up the GUI
@@ -61,16 +80,19 @@ public class PhotoCatcher extends JPanel{
 		
 		//Adding the "Add a directory" button to the sidebar right at the top.
 		//Much of this section was found on the Oracle tutorial "How to Use GridBagLayout"
-		JButton button = new JButton("Add a directory");
+		JButton addDirButton = new JButton("Add a directory");
+		AddDirectoryListener dirListener = new AddDirectoryListener();
+		addDirButton.addActionListener(dirListener);
 		sideBarConstraints.fill = GridBagConstraints.HORIZONTAL;
 		sideBarConstraints.gridheight = 1;
 		sideBarConstraints.gridx = 0;
 		sideBarConstraints.gridy = 0;
-		sideBar.add(button, sideBarConstraints);
+		sideBar.add(addDirButton, sideBarConstraints);
 		
 		//Directories list created and added to side bar below the button.
 		dirList= new JTextArea("No directories selected.");
-		sideBarConstraints.fill = GridBagConstraints.VERTICAL;
+		dirList.setEditable(false);
+		sideBarConstraints.fill = GridBagConstraints.BOTH;
 		sideBarConstraints.ipady = 40;      //make this component tall
 		sideBarConstraints.ipadx = 70;
 		sideBarConstraints.weighty = 10.0; //Makes sure it takes up as much vertical free space as possible.
@@ -82,24 +104,7 @@ public class PhotoCatcher extends JPanel{
 		
 		//Creates the JPanel for the settings.
 		settingsPanel = new JPanel();
-		settingsPanel.setLayout(new GridBagLayout());
-		GridBagConstraints settingsConstraints = new GridBagConstraints();
-		settingsConstraints.insets = new Insets(10,10,10,10);
-		settingsConstraints.anchor = GridBagConstraints.PAGE_END;
-		
-		
-		//Add the settings title
-		JLabel settingsTitle = new JLabel("Settings", SwingConstants.CENTER);
-		settingsConstraints.fill = GridBagConstraints.NORTH;
-		settingsConstraints.weightx = 1.0;
-		settingsConstraints.gridheight = 1;
-		settingsConstraints.gridwidth = 2;
-		settingsConstraints.gridx = 0;
-		settingsConstraints.gridy = 0;
-		settingsPanel.add(settingsTitle, settingsConstraints);
-		
-		
-		
+		settingsPanel.setLayout(new GridLayout(1,0));
 		
 		
 		//Create the extensions JPanel
@@ -111,7 +116,9 @@ public class PhotoCatcher extends JPanel{
 		//Adds the "Extensions" title into the extensions panel.
 		JLabel extensionsTitle = new JLabel("Extensions", SwingConstants.CENTER);
 		extensionsConstraints.fill = GridBagConstraints.HORIZONTAL;
+		extensionsConstraints.ipady = 1;
 		extensionsConstraints.weightx = 1.0;
+		extensionsConstraints.weighty = 0.0;
 		extensionsConstraints.gridwidth = 2;
 		extensionsConstraints.gridx = 0;
 		extensionsConstraints.gridy = 0;
@@ -119,13 +126,13 @@ public class PhotoCatcher extends JPanel{
 		
 		
 		//Creates a Text Area for the listed extensions and adds that to the extensions JPanel.
-		extList= new JTextArea("No extensions.");
-		extensionsConstraints.fill = GridBagConstraints.HORIZONTAL;
-		extensionsConstraints.ipady = 10;      //make this component tall
-		extensionsConstraints.ipadx = 70;
+		extList = new JTextArea("No extensions.");
+		extList.setEditable(false);
+		extensionsConstraints.fill = GridBagConstraints.BOTH;
+		extensionsConstraints.ipady = 190;      //make this component tall
 		extensionsConstraints.weightx = 1.0;
 		extensionsConstraints.weighty = 20.0;
-		extensionsConstraints.gridheight = 1;
+		extensionsConstraints.gridheight = 3;
 		extensionsConstraints.gridwidth = 2;
 		extensionsConstraints.gridx = 0;
 		extensionsConstraints.gridy = 1;
@@ -136,13 +143,27 @@ public class PhotoCatcher extends JPanel{
 		//text box because apparently GridBagLayout just doesn't want to work on something like this.
 		JPanel theStupidButtonPanelBecauseJavaSaysItDoesntWorkAnyOtherWayScrewJavaMan = new JPanel();
 		theStupidButtonPanelBecauseJavaSaysItDoesntWorkAnyOtherWayScrewJavaMan.setLayout(new GridLayout(0,2));
+		
 		addExtension = new JButton("+");
+		AddExtensionListener addExt = new AddExtensionListener();
+		addExtension.addActionListener(addExt);
+		
 		removeExtension = new JButton("-");
+		RemoveExtensionListener removeExt = new RemoveExtensionListener();
+		removeExtension.addActionListener(removeExt);
+		
+		//Patchnote (4/26/19) - I may have found out what I did wrong. Yes Java, what /I/ did wrong.
+		//Please come back. I didn't mean what I said. We can make awesome GUIs together using your admittedly
+		//confusing and convoluted GridBagLayout, but still understanding how we did it the whole time.
 		theStupidButtonPanelBecauseJavaSaysItDoesntWorkAnyOtherWayScrewJavaMan.add(addExtension);
 		theStupidButtonPanelBecauseJavaSaysItDoesntWorkAnyOtherWayScrewJavaMan.add(removeExtension);
 		
 		//Adds the dang JPanel to the extensions panel.
+		extensionsConstraints.fill = GridBagConstraints.HORIZONTAL;
 		extensionsConstraints.gridwidth = 2;
+		extensionsConstraints.ipady = 10;
+		extensionsConstraints.weightx = 1.0;
+		extensionsConstraints.weighty = 0.0;
 		extensionsConstraints.gridx = 0;
 		extensionsConstraints.gridy = 4;
 		extensionsConstraints.anchor = GridBagConstraints.PAGE_END;
@@ -159,53 +180,82 @@ public class PhotoCatcher extends JPanel{
 		
 		//Create the extensions JPanel
 		JPanel toggleSettingPanel = new JPanel();
-		toggleSettingPanel.setLayout(new GridLayout(0,10));
+		toggleSettingPanel.setLayout(new GridLayout(9,0));
 		
-		//toggleSettingPanel.add(new JLabel("This is a placeholder for the settings."));
+		//Create the toggle settings JPanel
+		toggleSettingPanel.add(new JLabel("Search Type"));
+		searchType = new JComboBox<>(searchTypeStrings);
+		ChangeSearchType changeSearchType = new ChangeSearchType();
+		searchType.addChangeListener(changeSearchType);
+		toggleSettingPanel.add(searchType);
+		
+		toggleSettingPanel.add(new JLabel("Algorithm"));
+		algorithmType = new JComboBox<>(algorithmTypeOptions[algorithmTypeInt]);
+		toggleSettingPanel.add(algorithmType);
+		
+		toggleSettingPanel.add(new JLabel("RGB Grace Value: 30"));
+		rgbGraceValue = new JSlider(JSlider.HORIZONTAL, 0, 255, 30);
+		//rgbGraceValue.addChangeListener();
+		toggleSettingPanel.add(rgbGraceValue);
 		
 		
+		toggleSettingPanel.add(new JLabel("Percent Similarity for Match: 60%"));
+		percentSimilarity = new JSlider(JSlider.HORIZONTAL, 0, 100, 60);
+		//percentSimilarity.addChangeListener();
+		toggleSettingPanel.add(percentSimilarity);
 		
-		
-		
-		
+		hardCompare = new JCheckBox("Hard Compare");
+		//hardCompare.addItemListener();
+		toggleSettingPanel.add(hardCompare);
 		
 		//If all else fails, I could always do two grid layouts: one that holds the Settings title up top, followed by the two settings planels in another grid layout.
+		JPanel actualSettingsPanel = new JPanel();
+		actualSettingsPanel.setLayout(new GridLayout(0,2));
+		
+		actualSettingsPanel.add(extensionsPanel);
+		actualSettingsPanel.add(toggleSettingPanel);
 		
 		
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		//Adds teh extensions panel to the settings panel.
-		settingsConstraints.fill = GridBagConstraints.WEST;
-		settingsConstraints.weightx = 1.0;
-		settingsConstraints.gridheight = 1;
-		settingsConstraints.gridx = 0;
-		settingsConstraints.gridy = 1;
-		settingsPanel.add(extensionsPanel, settingsConstraints);
-		
-		
+		settingsPanel.add(actualSettingsPanel);
 		
 		
 		
 		//Creates a start button. Easy!
 		JButton startButton = new JButton("Start");
+		StartListener startListener = new StartListener();
+		startButton.addActionListener(startListener);
 		
 		add(sideBar, BorderLayout.WEST);
-		add(toggleSettingPanel, BorderLayout.EAST);
 		add(settingsPanel, BorderLayout.CENTER);
 		add(startButton, BorderLayout.SOUTH);
+		
+		listOfDirectories = new ArrayList<File>();
+		
+		updateExtensions();
+		updateDirectories();
 	}
 	
 	public static void searchAndUpdate(){
-		File currentDirectory = new File(System.getProperty("user.dir"));
-		searchAllDirectories(currentDirectory);
+		
+		/* try{
+			//File directory = new File(System.getProperty("user.dir"));
+			//listOfDirectories.append(directory);
+		}
+		catch(Error e){
+			
+		} */
+		
+		
+		
+		
+		
+		//File currentDirectory = new File(System.getProperty("user.dir"));
+		for(int i=0; i<listOfDirectories.size(); i++){
+			searchAllDirectories(listOfDirectories.get(i));
+		}
 		
 		clearFileDuplicates();
 		catchThePhotoDuplicates();
@@ -228,8 +278,8 @@ public class PhotoCatcher extends JPanel{
 				boolean isAnImage = false;
 				
 				//Verify that the file extension is one of the listed extensions for the class.
-				for(int j=0; j<listOfPictureFiletypes.length; j++){
-					if(filetype.toLowerCase().equals(listOfPictureFiletypes[j])){
+				for(int j=0; j<listOfPictureFiletypes.size(); j++){
+					if(filetype.toLowerCase().equals(listOfPictureFiletypes.get(j))){
 						isAnImage = true;
 						break;
 					}
@@ -289,6 +339,119 @@ public class PhotoCatcher extends JPanel{
 			}
 		}
 	}
+	
+	
+	
+	
+	
+	//Action listeners unite!
+	//TODO: Make this happen!
+	private class StartListener implements ActionListener{
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			try{
+				//Start the algorithm!
+			}
+			catch(Exception ex){
+				
+			}
+		}
+		
+	}
+	
+	private class AddExtensionListener implements ActionListener{
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			try{
+				String newFiletype = JOptionPane.showInputDialog(new JFrame(), "Enter the file extension.", "Photo Catcher", JOptionPane.WARNING_MESSAGE);
+				
+				if(!newFiletype.equals("") && !newFiletype.equals("null") && newFiletype!=null){
+					listOfPictureFiletypes.add(newFiletype);
+				}
+				
+				updateExtensions();
+			}
+			catch(Exception ex){
+				
+			}
+		}
+		
+	}
+	
+	private class RemoveExtensionListener implements ActionListener{
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			try{
+				listOfPictureFiletypes.remove(listOfPictureFiletypes.size() - 1);
+				
+				updateExtensions();
+			}
+			catch(Exception ex){
+				
+			}
+		}
+		
+	}
+	
+	private void updateExtensions(){
+		String output = "";
+		String newline = "\n";
+		
+		for(int i=0; i<listOfPictureFiletypes.size(); i++){
+			output += listOfPictureFiletypes.get(i) + newline;
+		}
+		
+		extList.setText(null);
+		extList.append(output);
+	}
+	
+	private void updateDirectories(){
+		String output = "";
+		String newline = "\n";
+		
+		for(int i=0; i<listOfDirectories.size(); i++){
+			output += listOfDirectories.get(i) + newline;
+		}
+		
+		dirList.setText(null);
+		dirList.append(output);
+	}
+	
+	//TODO: Make this happen!
+	private class AddDirectoryListener implements ActionListener{
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			try{
+				//JFC for this one. See PowerPointKnockOff.
+				//Make sure not to accept a non-directory!
+			}
+			catch(Exception ex){
+				
+			}
+		}
+		
+	}
+	
+	//TODO: Make this happen! It's for the dropdown changing.
+	private class ChangeSearchType implements ChangeListener{
+		
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			try{
+				algorithmTypeInt = searchType.getSelectedIndex();
+				algorithmType = new JComboBox<>(algorithmTypeOptions[algorithmTypeInt]);
+			}
+			catch(Exception ex){
+				
+			}
+		}
+		
+	}
+	
 }
 
 
@@ -309,4 +472,9 @@ https://docs.oracle.com/javase/tutorial/uiswing/layout/gridbag.html
 https://docs.oracle.com/javase/tutorial/uiswing/layout/grid.html
 https://stackoverflow.com/questions/15455655/adding-buttons-below-jtextarea
 
+https://docs.oracle.com/javase/tutorial/uiswing/components/combobox.html
+https://docs.oracle.com/javase/tutorial/uiswing/components/slider.html
+https://stackoverflow.com/questions/18162985/found-raw-type-jcombobox
+https://docs.oracle.com/javase/tutorial/uiswing/components/button.html
+https://docs.oracle.com/javase/7/docs/api/javax/swing/event/ChangeListener.html
 */
